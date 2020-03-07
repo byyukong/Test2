34b50ead1ad1127f94e471c0982e69d2ad19e408
yukong <1719549608@qq.com>
2020-03-07 14:14:25 +0800

    第一次提交

---
+++ Test20/src/Test.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+
+/**
+ * @author GaoJianJun
+ * @create 2020-03-05 19:14
+ */
+public class Test {
+    static int a=5,b=6;
+    public static void main(String[] args) {
+
+    }
+}
